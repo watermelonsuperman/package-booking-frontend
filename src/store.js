6a5593b32003335b3e205c2e6835b3f4939d4424
watermelonsuperman <15538287230@163.com>
2019-09-05 21:14:19 +0800

    包裹入库 前端

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
@@ -11,6 +12,8 @@ export default new Vuex.Store({
 
   },
   actions: {
-
+    addMsg (store, form) {
+      axios.post('http://localhost:8080/packages', form)
+    }
   }
 })
---
+++ src/views/PackageIn.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <h1>包裹入库</h1>
+    <a-form :form="form" @submit="handleSubmit">
+      <a-form-item label="运单号" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }">
+        <a-input
+          v-decorator="[
+'number',
+{rules: [{ required: true, message: 'Please input your 运单号!' }]}
+]"
+        />
+      </a-form-item>
+      <a-form-item label="收件人" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }">
+        <a-input
+          v-decorator="[
+'name',
+{rules: [{ required: true, message: 'Please input your 收件人!' }]}
+]"
+        />
+      </a-form-item>
+      <a-form-item label="电话" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }">
+        <a-input
+          v-decorator="[
+'phone',
+{rules: [{ required: true, message: 'Please input your 电话!' }]}
+]"
+        />
+      </a-form-item>
+      <a-form-item label="重量" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }">
+        <a-input
+          v-decorator="[
+'weight',
+{rules: [{ required: true, message: 'Please input your 重量!' }]}
+]"
+        />
+      </a-form-item>
+      <a-form-item :wrapper-col="{ span: 12, offset: 5 }">
+        <a-button type="primary" html-type="submit">Submit</a-button>
+      </a-form-item>
+    </a-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formLayout: "horizontal",
+      form: this.$form.createForm(this)
+    };
+  },
+  methods: {
+    handleSubmit(e) {
+      e.preventDefault();
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          window.console.log(values);
+          
+        }this.$store.dispatch("addMsg", values);
+      });
+    }
+  }
+};
+</script>
